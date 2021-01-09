95e38e9dea0da124495dcd5c3e8a5c059cae335e
salemm <s.e.jugr@gmail.com>
2021-01-09 17:33:05 -0600

    :alien: do the components

---
+++ blenreact1/src/components/SuperHero.js
@@ -0,0 +1,12 @@
+const SuperHero = (props) => {
+  console.log(props);
+  return (
+    <section>
+      <div>
+        <h1>Super Hereoe nombre</h1>
+      </div>
+    </section>
+  );
+};
+
+export default SuperHero;
