5d0fae68cc4e7109b9eecfb664108a65181f39de
salemm <s.e.jugr@gmail.com>
2021-01-25 19:03:01 -0600

    carro ejercicio done

--- blenreact1/src/App.js
+++ blenreact1/src/App.js
@@ -7,56 +7,45 @@ import Saludar from "./components/Saludar";
 import data from "./data/super.json";
 import Formulario from "./components/Formulario";
 import Layout from "./components/Layout";
+
 function App() {
   //* variable de la izquierda (guarda el valor)
   //* variable de la derecha ( es la que lo cambia)
-  const [name, setName] = useState("Scorpion");
-
-  useEffect(() => {
-    async function llamar() {
-      const res = await fetch("https://rickandmortyapi.com/api/character");
-      const lla = await res.json();
-      console.log(lla);
-    }
-    llamar();
-  }, []);
-
-  const alumno = {
-    nombre: "kain",
-    edad: 37,
-    hobbie: "beber sangre de mis enemigos",
-  };
-  const saludarFunc = (name, hob) => {
-    alert(`${name} le mama hacer.${hob}  amen.`);
-  };
-
-  /*
-   * <img src={logo} className="App-logo" alt="logo" />
-        <SuperHeroe name="Iron Man" /> 
-        <Saludar alumno={alumno} saludarFn={saludarFunc} />
-        <br />
-        <Formulario />
-   */
-  const handleChange = (event) => {
-    //este trae el evento con su valor que viene del input
-    console.log(event.target.value);
-    //* este es el hook que va a acambiar el estado
-    setName(event.target.value);
-  };
-  const handleSubmit = () => {};
+  const [isOn, setIsOn] = useState(false);
+  const [kmavanza, setKm] = useState(0);
 
   return (
     <>
       <div className="App">
         <header className="App-header">
-          <section>
+          <h1>holis</h1>
+          <div>
+            <h3>Carro</h3>
+            <span>MI Nave esta :{isOn ? "encendido" : "apagado"}</span>
+            <br />
+            <button
+              className={`${isOn ? "apagado" : "encendido"}`}
+              onClick={() => setIsOn(!isOn)}
+            >
+              {isOn ? "Apagar la nave" : "Encender la Nave"}
+            </button>
+            <button
+              onClick={() => setKm(kmavanza + 10)}
+              disabled={!isOn ? true : false}
+            >
+              A darle gas
+            </button>
+            <br />
+            <span>Haz avanzado : {kmavanza} km</span>
+          </div>
+          {/* <section>
             <h1>{name} ha realizado una fatality 💀 🔥</h1>
             <input type="text" onChange={handleChange} name="cambianombre" />
             <button onClick={handleSubmit}>
               Manda el nombre del personaje a la base
             </button>
           </section>
-          <Saludar alumno={alumno} saludarF={saludarFunc} />
+          <Saludar alumno={alumno} saludarF={saludarFunc} /> */}
         </header>
       </div>
     </>
